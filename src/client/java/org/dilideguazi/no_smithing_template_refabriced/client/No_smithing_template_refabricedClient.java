523a75961c997aabad1de288562aec090183f013
Dilideguazi <pxc628142122022@outlook.com>
2026-03-21 17:24:40 +0800

    initial commit

---
+++ src/client/java/org/dilideguazi/no_smithing_template_refabriced/client/No_smithing_template_refabricedClient.java
@@ -0,0 +1,10 @@
+package org.dilideguazi.no_smithing_template_refabriced.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class No_smithing_template_refabricedClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
