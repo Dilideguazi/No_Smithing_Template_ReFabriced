52242d3b26b49c74e9983a6479329c4936b0e0b5
Dilideguazi <pxc628142122022@outlook.com>
2026-03-21 21:45:06 +0800

    initial commit

---
+++ src/main/java/org/dilideguazi/no_smithing_template_refabriced/No_smithing_template_refabriced.java
@@ -0,0 +1,23 @@
+package org.dilideguazi.no_smithing_template_refabriced;
+
+import fuzs.forgeconfigapiport.fabric.api.forge.v4.ForgeConfigRegistry;
+import net.fabricmc.api.ModInitializer;
+import net.neoforged.fml.config.ModConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class No_smithing_template_refabriced implements ModInitializer {
+    public static final String MOD_ID = "no_smithing_template_refabriced";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        ForgeConfigRegistry.INSTANCE.register(
+                MOD_ID,
+                ModConfig.Type.COMMON,
+                Config.SPEC
+        );
+
+        LOGGER.info("No Smithing Template ReFabriced Mod Loaded - Ready to craft without templates!");
+    }
+}
